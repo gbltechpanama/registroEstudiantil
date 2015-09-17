ac3e0a954413e305816b2f60eaec0cd83176bf07
ricardo <ricardo@PC01>
2015-09-17 19:26:51 -0430

    Estructura inicial

---
+++ registroEstudiantil/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <script type="text/javascript">
+            function redireccion()
+            {
+                    document.location.href = "";
+                    //prueba
+            }
+        </script>
+    </head>
+    <body onload="redireccion()">
+    </body>
+</html>
