8671b3a6c2ecb40b7c11cf85bf96133aa36c4f78
Ricardo Presilla <ricardopresilla@gmail.com>
2015-09-30 19:51:23 -0430

    Archivos necesarios para crear el calendario.

---
+++ registroEstudiantil/vista/js/calendarios.js
@@ -0,0 +1,56 @@
+/**Archivo necesario para crear los calendarios en la página web.
+ */
+window.onload = function(){
+    g_globalObject = new JsDatePick({
+            useMode:1,
+            isStripped:true,
+            target:"capaCalendario"
+            /*selectedDate:{
+                    day:5,
+                    month:9,
+                    year:2006
+            },
+            yearsRange:[1978,2020],
+            limitToToday:false,
+            cellColorScheme:"beige",
+            dateFormat:"%m-%d-%Y",
+            imgPath:"img/",
+            weekStartDay:1*/
+    });		
+
+    g_globalObject.setOnSelectedDelegate(function(){
+            var obj = g_globalObject.getSelectedDay();      
+            document.getElementById("fechaNacimiento").innerHTML = obj.day + "/" + obj.month + "/" + obj.year;
+            document.getElementById("resultado").innerHTML = obj.day + "/" + obj.month + "/" + obj.year;
+            document.getElementById("capaCalendario").style.display="none";
+    });
+
+    g_globalObject2 = new JsDatePick({
+            useMode:1,
+            isStripped:false,
+            target:"div4_example",
+            cellColorScheme:"beige"
+            /*selectedDate:{ 
+                    day:5,   
+                    month:9,
+                    year:2006
+            },
+            yearsRange:[1978,2020],
+            limitToToday:false,
+            dateFormat:"%m-%d-%Y",
+            imgPath:"img/",
+            weekStartDay:1*/
+    });
+
+    g_globalObject2.setOnSelectedDelegate(function(){
+            var obj = g_globalObject2.getSelectedDay();
+            alert("una fecha apenas se ha seleccionado y la fecha es : " + obj.day + "/" + obj.month + "/" + obj.year);
+            document.getElementById("fechaNacimiento").innerHTML = obj.day + "/" + obj.month + "/" + obj.year;
+    });		
+
+};
+/**Para mostrar la capa con el calendario*/
+function mostrarCalendario()
+{
+    document.getElementById("capaCalendario").style.display="block";
+}
