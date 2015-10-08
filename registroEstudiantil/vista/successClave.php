2e84f977ab1f720a169f01abd6cec734329893dd
Ricardo Presilla <ricardopresilla@gmail.com>
2015-10-08 15:21:50 -0430

    Agregando a la vista successClave el formato.

--- registroEstudiantil/vista/successClave.php
+++ registroEstudiantil/vista/successClave.php
@@ -11,6 +11,9 @@
         <div class="barra2"></div>
         <div class="container">
             <main>
+                <div class="cambioAceptado"> 
+                    <a href="formLogin.php" class="enlace2">Ir al inicio</a>
+                </div>
         <?php
         // put your code here
         ?>
