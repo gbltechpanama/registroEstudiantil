2dd230de22238a37757f8d51aa2d5009e0772373
Ricardo Presilla <ricardopresilla@gmail.com>
2015-11-05 18:05:50 -0430

    Agregando datos de prueba

--- registroEstudiantil/backup/BD.sql
+++ registroEstudiantil/backup/BD.sql
@@ -35,3 +35,17 @@ INSERT INTO estudiantil.estudiantes (`cedulaEstudiante`, nombres, apellidos,
     `lugarTrabajo`, `cargoTrabajo`, `rutaFoto`) VALUES ('32435465', 'Luis', 
     'Perez', 'Caracas', '02121234567', 'lperez@gmail.com', '1994-06-15', 
     'Caracas', 'Muebleria el turco', 'Vendedor', '../vista/img/fotos/19651.jpg');
+
+INSERT INTO estudiantil.estudiantes (`cedulaEstudiante`, nombres, apellidos, 
+    direccion, telefono, email, `fechaNacimiento`, `lugarNacimiento`, 
+    `lugarTrabajo`, `cargoTrabajo`, `rutaFoto`) VALUES ('12345', 'Jose', 
+    'Gutierrez', 'Barcelona, calle 12', '02811234567', 
+    'josegutierrez@gmail.com', '1994-07-05', 'Barcelona', 'Taller mecÃ¡nico', 
+    'MecÃ¡nico', '../vista/img/fotos/37668.jpg');
+
+INSERT INTO estudiantil.estudiantes (`cedulaEstudiante`, nombres, apellidos, 
+    direccion, telefono, email, `fechaNacimiento`, `lugarNacimiento`, 
+    `lugarTrabajo`, `cargoTrabajo`, `rutaFoto`) VALUES ('12345670', 'Maria', 
+    'Gonzalez', 'CumanÃ¡', '02931234789', 'mgonzalez@gmail.com', '1988-05-11', 
+    'Carupano', 'Farmacia MariÃ±o', 'Farmaceuta', '../vista/img/fotos/72543.png');
+
